1450ea238be3eca7edb9f6b567cb41609075e9a8
Pranto Chakraborty <chakrabortypranto39@gmail.com>
2022-11-26 00:44:45 +0600

    get all categories using react query

--- mobile-planet/src/Pages/Dashboard/Category/Category/Category.js
+++ mobile-planet/src/Pages/Dashboard/Category/Category/Category.js
@@ -1,14 +1,27 @@
+import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import React, { useState } from 'react';
+import { Button, Image, Table } from 'react-bootstrap';
 import { useForm } from 'react-hook-form';
 import { FaArrowRight } from 'react-icons/fa';
 import { successToast } from '../../../../toast/Toaster';
+import Loading from '../../../Shared/Loading/Loading';
 
 const Category = () => {
 
     const [load, setLoad] = useState(false);
     const { register, formState: { errors }, handleSubmit } = useForm();
     const imgHostKey = process.env.REACT_APP_IMGBB_KEY;
+    const { data: categories, isLoading, refetch } = useQuery({
+        queryKey: ['category'],
+        queryFn: () => fetch(`http://localhost:5000/category`, {
+            headers: {
+                authoraization: `bearer ${localStorage.getItem('mobile-planet')}`
+            }
+        }).then(res => res.json())
+
+    })
+
     const handleAddCategory = data => {
         setLoad(true)
         const img = data.img[0];
@@ -41,14 +54,23 @@ const Category = () => {
                             if (res.data.acknowledged) {
                                 successToast('Category Successfully Added !')
                                 setLoad(false)
+                                refetch()
                             }
                         })
                 }
             })
+
     }
+
+
+    if (isLoading) {
+        return <Loading></Loading>
+    }
+
     return (
         <>
             <h3>Category</h3>
+            {/* Add-category-form  */}
             <div className='shadow-lg p-3 my-3 bg-body rounded col-6 mx-auto'>
                 <h4>Add a Category</h4>
                 <form className='my-4' onSubmit={handleSubmit(handleAddCategory)}>
@@ -86,6 +108,36 @@ const Category = () => {
                         </div>
                     </button>
                 </form>
+            </div>
+
+            <div className='shadow-lg p-3 my-3 bg-body rounded'>
+                <h4 className='mb-5'>All Category</h4>
+                <Table striped>
+                    <thead>
+                        <tr>
+
+                            <th>#</th>
+                            <th>Category Name</th>
+                            <th>Category Image</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {categories.length > 0 && categories?.map((cat, i) =>
+                            <tr key={i}>
+                                <td>{i + 1}</td>
+
+                                <td>{cat.categoryName}</td>
+                                <td>
+                                    <Image roundedCircle style={{ height: '48px' }} src={cat.categoryImage} />
+                                </td>
+                                <td>
+                                    <Button variant='danger'>Delete</Button>
+                                </td>
+                            </tr>
+                        )}
+                    </tbody>
+                </Table>
             </div>
         </>
     );
