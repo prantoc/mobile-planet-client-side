128bbff47b7148d53c43419ad454f1c7367ab76f
Pranto Chakraborty <chakrabortypranto39@gmail.com>
2022-11-26 01:49:33 +0600

    modal setup

--- mobile-planet/src/Pages/Dashboard/Category/Category/Category.js
+++ mobile-planet/src/Pages/Dashboard/Category/Category/Category.js
@@ -1,14 +1,16 @@
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import React, { useState } from 'react';
-import { Button, Image, Table } from 'react-bootstrap';
+import { Button, Image, Modal, Table } from 'react-bootstrap';
 import { useForm } from 'react-hook-form';
-import { FaArrowRight } from 'react-icons/fa';
+import { FaPlus } from 'react-icons/fa';
 import { deleteItemAlret, successToast, swlFire } from '../../../../toast/Toaster';
 import Loading from '../../../Shared/Loading/Loading';
 
 const Category = () => {
-
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
     const [load, setLoad] = useState(false);
     const { register, formState: { errors }, handleSubmit, reset } = useForm();
     const imgHostKey = process.env.REACT_APP_IMGBB_KEY;
@@ -52,6 +54,7 @@ const Category = () => {
                     axios.post('http://localhost:5000/add-category', category, config)
                         .then(res => {
                             if (res.data.acknowledged) {
+                                setShow(false)
                                 successToast('Category Successfully Added !')
                                 setLoad(false)
                                 refetch()
@@ -93,48 +96,14 @@ const Category = () => {
     return (
         <>
             <h3>Category</h3>
-            {/* Add-category-form  */}
-            <div className='shadow-lg p-3 my-3 bg-body rounded col-6 mx-auto'>
-                <h4>Add a Category</h4>
-                <form className='my-4' onSubmit={handleSubmit(handleAddCategory)}>
-                    <div className="mb-4">
-                        <label htmlFor="exampleInputName" className="form-label">Category name</label>
-                        <input {...register("name", {
-                            required: { value: true, message: "Category name is required" },
-                        })} type="text" className="form-control" id="exampleInputName" aria-invalid={errors.name ? "true" : "false"} />
-                        {errors.name && <p className='text-danger fw-bold my-1' role="alert">{errors.name?.message}</p>}
-                    </div>
 
-                    <div className="mb-4">
-                        <label htmlFor="exampleInputImg" className="form-label">Category Image</label>
-                        <input {...register("img", {
-                            required: { value: true, message: "Profile Photo is required" },
-                            validate: {
-                                lessThan10MB: (files) => files[0]?.size < 10000000 || "Max 10MB",
-                                acceptedFormats: (files) =>
-                                    ["image/jpeg", "image/png", "image/gif"].includes(files[0]?.type) ||
-                                    "Only PNG, JPEG e GIF",
-                            },
-                        })} type="file" className="form-control" id="exampleInputImg" aria-invalid={errors.img ? "true" : "false"} />
-                        {errors.img && <p className='text-danger fw-bold my-1' role="alert">{errors.img?.message}</p>}
-                    </div>
-
-                    <button type="submit" className="btn btn-primary text-center col-12  rounded">
-                        <div>
-                            {load
-                                ?
-                                <div className="spinner-border text-dark" role="status">
-                                    <span className="visually-hidden">Loading...</span>
-                                </div>
-                                :
-                                <>Add a Category <FaArrowRight></FaArrowRight></>}
-                        </div>
-                    </button>
-                </form>
-            </div>
 
             <div className='shadow-lg p-3 my-3 bg-body rounded'>
-                <h4 className='mb-5'>All Category</h4>
+                <div className="d-flex justify-content-end align-items-center mb-5">
+                    <Button variant="primary" onClick={handleShow}>
+                        Add a Category <FaPlus></FaPlus>
+                    </Button>
+                </div>
                 <Table striped>
                     <thead>
                         <tr>
@@ -165,6 +134,54 @@ const Category = () => {
                     </tbody>
                 </Table>
             </div>
+
+
+            <Modal show={show} onHide={handleClose}>
+                <div className='shadow-lg bg-body'>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Add a Category</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+
+                        <form className='my-4' onSubmit={handleSubmit(handleAddCategory)}>
+                            <div className="mb-4">
+                                <label htmlFor="exampleInputName" className="form-label">Category name</label>
+                                <input {...register("name", {
+                                    required: { value: true, message: "Category name is required" },
+                                })} type="text" className="form-control" id="exampleInputName" aria-invalid={errors.name ? "true" : "false"} />
+                                {errors.name && <p className='text-danger fw-bold my-1' role="alert">{errors.name?.message}</p>}
+                            </div>
+
+                            <div className="mb-4">
+                                <label htmlFor="exampleInputImg" className="form-label">Category Image</label>
+                                <input {...register("img", {
+                                    required: { value: true, message: "Profile Photo is required" },
+                                    validate: {
+                                        lessThan10MB: (files) => files[0]?.size < 10000000 || "Max 10MB",
+                                        acceptedFormats: (files) =>
+                                            ["image/jpeg", "image/png", "image/gif"].includes(files[0]?.type) ||
+                                            "Only PNG, JPEG e GIF",
+                                    },
+                                })} type="file" className="form-control" id="exampleInputImg" aria-invalid={errors.img ? "true" : "false"} />
+                                {errors.img && <p className='text-danger fw-bold my-1' role="alert">{errors.img?.message}</p>}
+                            </div>
+
+                            <button type="submit" className="btn btn-primary text-center col-12  rounded">
+                                <div>
+                                    {load
+                                        ?
+                                        <div className="spinner-border text-dark" role="status">
+                                            <span className="visually-hidden">Loading...</span>
+                                        </div>
+                                        :
+                                        <>Add a Category <FaPlus></FaPlus></>}
+                                </div>
+                            </button>
+                        </form>
+
+                    </Modal.Body>
+                </div>
+            </Modal>
         </>
     );
 };
