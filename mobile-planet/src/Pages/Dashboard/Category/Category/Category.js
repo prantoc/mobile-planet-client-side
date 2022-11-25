0051f4ad2ef0d1eb1d1ab5e8583fdbbc51b885cc
Pranto Chakraborty <chakrabortypranto39@gmail.com>
2022-11-26 00:26:12 +0600

    store category using axios

--- mobile-planet/src/Pages/Dashboard/Category/Category/Category.js
+++ mobile-planet/src/Pages/Dashboard/Category/Category/Category.js
@@ -1,28 +1,15 @@
-import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { FaArrowRight } from 'react-icons/fa';
-import { useNavigate } from 'react-router-dom';
 import { successToast } from '../../../../toast/Toaster';
-import Loading from '../../../Shared/Loading/Loading';
 
 const Category = () => {
 
     const [load, setLoad] = useState(false);
     const { register, formState: { errors }, handleSubmit } = useForm();
-    const navigate = useNavigate()
-    const imgHostKey = process.env.REACT_APP_imgbb_key;
-    // const { data: specialities, isLoading } = useQuery({
-    //     queryKey: ['speciality'],
-    //     queryFn: () => fetch(`https://doctors-portal-server-theta.vercel.app/appointmentSpeciality`, {
-    //         headers: {
-    //             authoraization: `bearer ${localStorage.getItem('doctor-portal')}`
-    //         }
-    //     }).then(res => res.json())
-
-    // })
-
-    const handleAddDoctor = data => {
+    const imgHostKey = process.env.REACT_APP_IMGBB_KEY;
+    const handleAddCategory = data => {
         setLoad(true)
         const img = data.img[0];
         const formData = new FormData();
@@ -35,48 +22,36 @@ const Category = () => {
             .then(res => res.json())
             .then(imgData => {
                 if (imgData.success) {
-                    const { name, email, speciality } = data
-                    const avatar = imgData.data.url
-                    const doctor = {
-                        name,
-                        email,
-                        speciality,
-                        avatar
+                    const { name } = data
+                    const categoryImage = imgData.data.url
+                    const category = {
+                        categoryName: name,
+                        categoryImage
                     }
 
-                    fetch(`https://doctors-portal-server-theta.vercel.app/addDoctors`, {
-                        method: 'POST',
+                    const config = {
                         headers: {
                             'content-type': 'application/json',
-                            authoraization: `bearer ${localStorage.getItem('doctor-portal')}`
-                        },
-                        body: JSON.stringify(doctor)
-                    })
-                        .then(res => res.json())
-                        .then(data => {
-                            if (data.acknowledged) {
-                                successToast('Doctor Successfully Added !')
+                            authoraization: `bearer ${localStorage.getItem('mobile-planet')}`
+                        }
+                    }
+
+                    axios.post('http://localhost:5000/add-category', category, config)
+                        .then(res => {
+                            if (res.data.acknowledged) {
+                                successToast('Category Successfully Added !')
                                 setLoad(false)
                             }
                         })
-
                 }
             })
-
     }
-
-
-    // if (isLoading) {
-    //     return <Loading></Loading>
-    // }
-
-
     return (
         <>
             <h3>Category</h3>
             <div className='shadow-lg p-3 my-3 bg-body rounded col-6 mx-auto'>
                 <h4>Add a Category</h4>
-                <form className='my-4' onSubmit={handleSubmit(handleAddDoctor)}>
+                <form className='my-4' onSubmit={handleSubmit(handleAddCategory)}>
                     <div className="mb-4">
                         <label htmlFor="exampleInputName" className="form-label">Category name</label>
                         <input {...register("name", {
@@ -89,6 +64,12 @@ const Category = () => {
                         <label htmlFor="exampleInputImg" className="form-label">Category Image</label>
                         <input {...register("img", {
                             required: { value: true, message: "Profile Photo is required" },
+                            validate: {
+                                lessThan10MB: (files) => files[0]?.size < 10000000 || "Max 10MB",
+                                acceptedFormats: (files) =>
+                                    ["image/jpeg", "image/png", "image/gif"].includes(files[0]?.type) ||
+                                    "Only PNG, JPEG e GIF",
+                            },
                         })} type="file" className="form-control" id="exampleInputImg" aria-invalid={errors.img ? "true" : "false"} />
                         {errors.img && <p className='text-danger fw-bold my-1' role="alert">{errors.img?.message}</p>}
                     </div>
